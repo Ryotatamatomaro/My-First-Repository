a97c38f0afbcd88d9bae3b102fd28fbbad400a0c
Ryotatamatomaro <ryotai71104@yahoo.co.jp>
2024-05-23 22:31:20 +0900

    commit of curriculum8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+
 
 class PostController extends Controller
 {
-    //
+    public function index(Post $post)
+    {
+       return view('posts.index')->with(['posts' => $post->get()]);//$postの中身を戻り値にする
+    }
 }
