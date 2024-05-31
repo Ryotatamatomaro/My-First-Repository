842eee00e72c36152321df41f12c1140a43bef97
Ryotatamatomaro <ryotai71104@yahoo.co.jp>
2024-06-01 01:23:38 +0900

    completion of commit of curriculum 8-4

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
 
-Route::get('/posts', [PostController::class, 'index']);   
+// Route::get('/posts', [PostController::class, 'index']);   
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,6 @@ Route::get('/posts', [PostController::class, 'index']);
 */
 
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store' ]);
